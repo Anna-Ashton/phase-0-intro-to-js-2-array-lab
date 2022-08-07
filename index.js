855355f9d8db29cabc88e19dba9678ebc6171e3a
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-08 01:51:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function logcats(names) {
+    console.log("cats:", names);
+}
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(Garfield) {
+cats.pop(Garfield);
+}
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+function appendCat(name) {
+    return[...cats, name];
+}
+function prependCat(name) {
+    return[name, ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
